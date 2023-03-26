b2751196166698f8501faf95578a45a6a75dc857
Shariful Alam <shariful.alam85@gmail.com>
2023-03-27 02:00:45 +0600

    Created Chirp resource and added create functionality for Chirp resource.

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+
+class ChirpController extends Controller
+{
+    public function index(): View
+    {
+        return view('chirps.index');
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+           'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Chirp  $chirp
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Chirp  $chirp
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Chirp  $chirp
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Chirp  $chirp
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Chirp $chirp)
+    {
+        //
+    }
+}
--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,10 @@
-@props(['disabled' => false])
+@props([
+  'disabled' => false,
+  'error' => false,
+  ])
+
+@if ($error)
+    <div class="mt-1 text-red-500 text-sm">{{ $error }}</div>
+@endif
 
 <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
